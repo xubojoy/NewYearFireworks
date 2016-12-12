678dc4d0376f6a22f26cb083bf68770340c60f27
xubojoy <xubojoy1112@163.com>
2016-12-12 13:11:50 +0800

    烟花

---
+++ test/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by     songguolin on 15/12/30.
+//  Copyright © 2015年 innos-campus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "YFGIFImageView.h"
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+@property (weak, nonatomic) IBOutlet YFGIFImageView *imgv;
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,191 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by     songguolin on 15/12/30.
+//  Copyright © 2015年 innos-campus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    AVAudioPlayer *avAudioPlayer;
+}
+@property (nonatomic,assign) NSInteger tapNum;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tapNum=1;
+    self.button.layer.cornerRadius=50;
+    self.button.layer.masksToBounds=YES;
+    // 雪花／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／
+    
+    // Configure the particle emitter to the top edge of the screen
+    CAEmitterLayer *snowEmitter = [CAEmitterLayer layer];
+    snowEmitter.emitterPosition = CGPointMake(self.view.bounds.size.width / 2.0, -30);
+    snowEmitter.emitterSize		= CGSizeMake(self.view.bounds.size.width * 2.0, 0.0);;
+    
+    // Spawn points for the flakes are within on the outline of the line
+    snowEmitter.emitterMode		= kCAEmitterLayerOutline;
+    snowEmitter.emitterShape	= kCAEmitterLayerLine;
+    
+    // Configure the snowflake emitter cell
+    CAEmitterCell *snowflake = [CAEmitterCell emitterCell];
+    
+    //    随机颗粒的大小
+    snowflake.scale = 0.2;
+    snowflake.scaleRange = 0.5;
+    
+    //    缩放比列速度
+    //        snowflake.scaleSpeed = 0.1;
+    
+    //    粒子参数的速度乘数因子；
+    snowflake.birthRate		= 20.0;
+    
+    //生命周期
+    snowflake.lifetime		= 60.0;
+    
+    //    粒子速度
+    snowflake.velocity		= 20;				// falling down slowly
+    snowflake.velocityRange = 10;
+    //    粒子y方向的加速度分量
+    snowflake.yAcceleration = 2;
+    
+    //    周围发射角度
+    snowflake.emissionRange = 0.5 * M_PI;		// some variation in angle
+    //    自动旋转
+    snowflake.spinRange		= 0.25 * M_PI;		// slow spin
+    
+    snowflake.contents		= (id) [[UIImage imageNamed:@"snow"] CGImage];
+    snowflake.color			= [[UIColor colorWithRed:0.600 green:0.658 blue:0.743 alpha:1.000] CGColor];
+    
+    // Make the flakes seem inset in the background
+    snowEmitter.shadowOpacity = 1.0;
+    snowEmitter.shadowRadius  = 0.0;
+    snowEmitter.shadowOffset  = CGSizeMake(0.0, 1.0);
+    snowEmitter.shadowColor   = [[UIColor whiteColor] CGColor];
+    
+    // Add everything to our backing layer below the UIContol defined in the storyboard
+    snowEmitter.emitterCells = [NSArray arrayWithObject:snowflake];
+    [self.view.layer addSublayer:snowEmitter];
+    
+    //    [self.view.layer insertSublayer:snowEmitter atIndex:0];
+    //// 雪花／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／
+    //// 雪花／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／／
+    // Cells spawn in the bottom, moving up
+    CAEmitterLayer *fireworksEmitter = [CAEmitterLayer layer];
+    CGRect viewBounds = self.view.layer.bounds;
+    fireworksEmitter.emitterPosition = CGPointMake(viewBounds.size.width/2.0, viewBounds.size.height);
+    fireworksEmitter.emitterSize	= CGSizeMake(viewBounds.size.width/2.0, 0.0);
+    fireworksEmitter.emitterMode	= kCAEmitterLayerOutline;
+    fireworksEmitter.emitterShape	= kCAEmitterLayerLine;
+    fireworksEmitter.renderMode		= kCAEmitterLayerAdditive;
+    fireworksEmitter.seed = (arc4random()%100)+1;
+    
+    // Create the rocket
+    CAEmitterCell* rocket = [CAEmitterCell emitterCell];
+    
+    rocket.birthRate		= 5.0;
+    rocket.emissionRange	= 0.25 * M_PI;  // some variation in angle
+    rocket.velocity			= 380;
+    rocket.velocityRange	= 380;
+    rocket.yAcceleration	= 75;
+    rocket.lifetime			= 1.02;	// we cannot set the birthrate < 1.0 for the burst
+    
+    rocket.contents			= (id) [[UIImage imageNamed:@"ball"] CGImage];
+    rocket.scale			= 0.2;
+//    rocket.color			= [[UIColor colorWithRed:1 green:0 blue:0 alpha:1] CGColor];
+    rocket.greenRange		= 1.0;		// different colors
+    rocket.redRange			= 1.0;
+    rocket.blueRange		= 1.0;
+    
+    rocket.spinRange		= M_PI;		// slow spin
+    
+    
+    
+    // the burst object cannot be seen, but will spawn the sparks
+    // we change the color here, since the sparks inherit its value
+    CAEmitterCell* burst = [CAEmitterCell emitterCell];
+    
+    burst.birthRate			= 1.0;		// at the end of travel
+    burst.velocity			= 0;
+    burst.scale				= 2.5;
+    burst.redSpeed			=-1.5;		// shifting
+    burst.blueSpeed			=+1.5;		// shifting
+    burst.greenSpeed		=+1.0;		// shifting
+    burst.lifetime			= 0.35;
+    
+    // and finally, the sparks
+    CAEmitterCell* spark = [CAEmitterCell emitterCell];
+    
+    spark.birthRate			= 400;
+    spark.velocity			= 125;
+    spark.emissionRange		= 2* M_PI;	// 360 deg
+    spark.yAcceleration		= 75;		// gravity
+    spark.lifetime			= 3;
+    
+    spark.contents			= (id) [[UIImage imageNamed:@"fire"] CGImage];
+    spark.scale		        =0.5;
+    spark.scaleSpeed		=-0.2;
+    spark.greenSpeed		=-0.1;
+    spark.redSpeed			= 0.4;
+    spark.blueSpeed			=-0.1;
+    spark.alphaSpeed		=-0.5;
+    spark.spin				= 2* M_PI;
+    spark.spinRange			= 2* M_PI;
+    
+    // putting it together
+    fireworksEmitter.emitterCells	= [NSArray arrayWithObject:rocket];
+    rocket.emitterCells				= [NSArray arrayWithObject:burst];
+    burst.emitterCells				= [NSArray arrayWithObject:spark];
+    [self.view.layer addSublayer:fireworksEmitter];
+  
+    NSString *string = [[NSBundle mainBundle] pathForResource:@"Sainkho Namtchylak - Lost Rivers" ofType:@"mp3"];
+    //把音频文件转换成url格式
+    NSURL *url = [NSURL fileURLWithPath:string];
+    //初始化音频类 并且添加播放文件
+    avAudioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    //设置代理
+    avAudioPlayer.delegate = self;
+    
+    //设置初始音量大小
+    // avAudioPlayer.volume = 1;
+    
+    //设置音乐播放次数  -1为一直循环
+    avAudioPlayer.numberOfLoops = -1;
+    
+    //预播放
+    [avAudioPlayer prepareToPlay];
+    [avAudioPlayer play]; //播放
+}
+
+- (IBAction)click:(id)sender {
+    
+    
+    if (self.tapNum==1) {
+        self.imgv.gifPath = [[NSBundle mainBundle] pathForResource:@"xia.gif" ofType:nil];
+        [self.imgv startGIF];
+    }
+   self.tapNum+=1;
+    
+    if (self.tapNum==4) {
+         [self.imgv stopGIF];
+        self.imgv.image= [UIImage imageNamed:@"21199438.jpg"];
+
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
